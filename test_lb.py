0ac10798f27aac6b5f84d9647b7605303306c8da
lbaldini <luca.baldini@pi.infn.it>
2019-03-08 11:01:27 +0100

    Initial import.

---
+++ test_lb.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2017, Luca Baldini.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+from rating import load_publication_list
+
+pubs = load_publication_list()
+
+pubs = pubs.select(author_name='LUCA', author_surname='BALDINI')
+print('Done, %d publication(s) selected.' % len(pubs))
+
+print('Calulating rating points...')
+rating = sum(pub.rating_points('a') for pub in pubs)
+print('Rating: %.3f' % rating)
+
+    
+
+
